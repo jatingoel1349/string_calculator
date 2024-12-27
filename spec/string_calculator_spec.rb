5af66d3d85aa958ddd37e1a989a6fbea60d760df
Jatin Goel <jatingoel13@gmail.com>
2024-12-28 01:20:17 +0530

    added sum of number with comma

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -10,4 +10,9 @@ RSpec.describe StringCalculator do
     calculator = StringCalculator.new
     expect(calculator.add("1")).to eq(1)
   end
+
+  it "returns the sum for two numbers" do
+    calculator = StringCalculator.new
+    expect(calculator.add("1,5")).to eq(6)
+  end
 end
