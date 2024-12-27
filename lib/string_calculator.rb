1aa55de8c8f1f97afaedab0f3cb5b5408fc12d7b
Jatin Goel <jatingoel13@gmail.com>
2024-12-28 01:36:19 +0530

    support dynamic delimeter to sum

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -2,6 +2,16 @@ class StringCalculator
   def add(numbers)
     return 0 if numbers.empty?
 
-    numbers.split(/,|\n/).map(&:to_i).sum
+    # define default delimiters: comma and newline
+    delimiter = /,|\n/ 
+
+    if numbers.start_with?("//")
+      parts = numbers.split("\n", 2)
+      # Extract the dynamic delimiter
+      delimiter = Regexp.escape(parts[0][2..]) 
+      numbers = parts[1]
+    end
+
+    numbers.split(/#{delimiter}/).map(&:to_i).sum
   end
 end
--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -25,4 +25,9 @@ RSpec.describe StringCalculator do
     calculator = StringCalculator.new
     expect(calculator.add("1\n2,3")).to eq(6)
   end
+
+  it "supports custom delimiters" do
+    calculator = StringCalculator.new
+    expect(calculator.add("//;\n1;2")).to eq(3)
+  end
 end
