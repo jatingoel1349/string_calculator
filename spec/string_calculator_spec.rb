ff04d1a726ea4be4d07c466f94afa0cc1fe6148e
Jatin Goel <jatingoel13@gmail.com>
2024-12-28 01:25:04 +0530

    Add test case to check multiple numbers

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -15,4 +15,9 @@ RSpec.describe StringCalculator do
     calculator = StringCalculator.new
     expect(calculator.add("1,5")).to eq(6)
   end
+
+  it "returns the sum for multiple numbers" do
+    calculator = StringCalculator.new
+    expect(calculator.add("1,2,3,4,5")).to eq(15)
+  end
 end
